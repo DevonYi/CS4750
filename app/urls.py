a05d308fa80093d8540a2cb6d9a18d5c342be234
jas5mg <jas5mg@virginia.edu>
2017-11-19 13:12:19 -0500

    Created home page

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
 from . import views
+from app.views import *
 
 
 urlpatterns = [
-
+    url(r'^$', home, name='home'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
+from datetime import datetime
 # Create your views here.
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'index.html',
+        context={
+            'title': 'Home Page',
+        }
+    )
