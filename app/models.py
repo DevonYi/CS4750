576a418e02d08d3dab333e29940c9d5b1ef426c4
sjl5bz <sjl5bz@virginia.edu>
2017-11-19 14:13:57 -0500

    adding navigation bar

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Hospital(models.Model):
+	hUid = models.IntegerField()
+	hName = models.IntegerField()
+	hCapacity = models.IntegerField()
+	hStreetAddress = models.IntegerField()
+	hCapacity = models.IntegerField()
+	hZipcode = models.IntegerField()
+	hStateCode = models.IntegerField()
